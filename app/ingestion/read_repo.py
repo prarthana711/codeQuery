4d292887a53b1d7960c6f1f2a3116673a19aa9e1
prarthana711 <dprarthana994@gmail.com>
2026-07-29 10:05:32 +0530

    Update read_repo logic for chunking

--- app/ingestion/read_repo.py
+++ app/ingestion/read_repo.py
@@ -9,13 +9,12 @@ for root, dirs, files in os.walk(repo_path):
         if file.endswith((".py", ".md", ".txt")):
 
             full_path = os.path.join(root, file)
-            print(full_path)
+            #print(full_path)
             try:
                 with open(full_path, "r",encoding="utf-8",errors="ignore") as f:
                     content = f.read()
                 chunks=chunk_text(content)
                 for chunk in chunks:
-
                     documents.append(
                         {
                             "path": full_path,
@@ -23,7 +22,6 @@ for root, dirs, files in os.walk(repo_path):
                         }
                     )
             except Exception as e:
-
                 print(e)
 print(len(documents))
 
